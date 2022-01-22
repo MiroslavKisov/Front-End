c4ad9515266fdb170d3eb0b18c393058a07a2cf7
MiroslavKisov <miromirkata@hotmail.com>
2022-01-22 17:34:36 +0200

    Completing challenge 1.

---
+++ JavaScript/Udemy/complete-javascript-course-master/01-Fundamentals-Part-1/challenge_001.js
@@ -0,0 +1,9 @@
+let markHeight = 1.69;
+let johnHeight = 1.95;
+let markMass = 78;
+let johnMass = 92;
+
+let markBMI = markMass / markHeight ** 2;
+let johnBMI = johnMass / johnHeight ** 2;
+
+console.log('Mark BMI: ' + markBMI + ' ' + 'JohnBMI: ' + johnBMI);
